28d043694794046ca48bf75eb2ae845b7756febe
Alexandre Akira <alexandreakao@edu.unifor.br>
2020-06-07 06:16:56 -0300

    point controller

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,20 @@
 import express from 'express';
 
+import knex from './database/connection';
+
+import PointController from './controllers/PointController';
+
 const routes = express.Router();
+const pointControllerInstance = new PointController();
 
-routes.get('/', (req, res) => {
+routes.get('/items', async (req, res) => {
+  const items = await knex('items').select('*');
 
-  res.json({ message: 'hello world' })
+  const serializedItems = items.map(item => ({ ...item, image_url: `http://localhost:3333/uploads/${item.image}` }));
+
+  return res.json(serializedItems);
 })
+
+routes.post('/points', pointControllerInstance.create);
 
 export default routes;
