3b0e2c8d70b63c8dcbc239547ae71a1a851b81d7
Alexandre Akira <alexandreakao@edu.unifor.br>
2020-06-08 11:50:57 -0300

    click map

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState, ChangeEvent } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from "react-icons/fi";
 import { Map, TileLayer, Marker } from 'react-leaflet';
+import { LeafletMouseEvent } from 'leaflet';
 import axios from 'axios';
 
 import api from '../../services/api';
@@ -27,11 +28,14 @@ interface IBGECityResponse {
 
 const CreatePoint = () => {
   const [items, setItems] = useState<Item[]>([]);
+
   const [ufs, setUfs] = useState<string[]>([]);
   const [selectedUf, setSelectedUf] = useState('0');
+
   const [cities, setCities] = useState<string[]>([]);
   const [selectedCity, setSelectedCity] = useState('0');
-  
+
+  const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
 
   useEffect(() => {
     api.get('items').then(response => {
@@ -72,6 +76,13 @@ const CreatePoint = () => {
     const city = event.target.value;
 
     setSelectedCity(city);
+  }
+
+  function handleMapClick(event: LeafletMouseEvent) {
+    setSelectedPosition([
+      event.latlng.lat,
+      event.latlng.lng
+    ])
   }
 
   return (
@@ -127,12 +138,12 @@ const CreatePoint = () => {
             <span>Selecione o endereço no mapa</span>
           </legend>
 
-          <Map center={[-3.7451746, -38.4744777]} zoom={15}>
+          <Map center={[-3.7451746, -38.4744777]} zoom={15} onClick={handleMapClick} >
             <TileLayer 
               attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[-3.7451746, -38.4744777]} />
+            <Marker position={selectedPosition} />
           </Map>
 
           <div className="field-group">
