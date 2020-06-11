eafb4b2ed6de9ca1bcf69c434e55d64fd309050d
Alexandre Akira <alexandreakao@edu.unifor.br>
2020-06-11 19:01:05 -0300

    point page

--- backend/src/controllers/ItemController.ts
+++ backend/src/controllers/ItemController.ts
@@ -6,7 +6,7 @@ class ItemController {
   async index(req: Request, res: Response) {
     const items = await knex('items').select('*');
 
-    const serializedItems = items.map(item => ({ ...item, image_url: `http://localhost:3333/uploads/${item.image}` }));
+    const serializedItems = items.map(item => ({ ...item, image_url: `http://10.0.0.100:3333/uploads/${item.image}` }));
   
     return res.json(serializedItems);
   }
